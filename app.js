bbbf6f69ab5b63ced9b3373eb3fbc5214ec8efdd
yassy <wiren0413@gmail.com>
2025-06-14 02:09:41 +0900

    refactor

--- app.js
+++ app.js
@@ -1,150 +1,126 @@
 "use strict";
 
-const API_URL = "https://api.mentemori.icu";
-const WORLD_ID_KEY = "world_id";
+class Api {
+  static API_URL = 'https://api.mentemori.icu';
 
-$(document).ready(async () => {
-  const rankingCellContainer1 = $('#ranking-cells-1');
-  const rankingCellContainer2 = $('#ranking-cells-2');
-  const rankingCellContainer3 = $('#ranking-cells-3');
-  const bpCellContainer = $('#bp-cells');
-  const submitButtonTextElement = $('#Submit i.material-icons');
-  const headerTitleElement = $('#header-title');
-  const guildNameElement = $('#guild-name');
-  const rankingCellTemplate = document.querySelector('#ranking-cell');
-  const worldIdInputField = $('#number_input');
-  const groupLabel = $('#group-label')
-  const contentExplain = $('#content-explain');
-  const contentRankings = $('#content-rankings');
-  const contentBpList = $('#content-bplist');
-
-  let rankingCellList = [];
-  let bpCellList = [];
-  let groups = {};
-  let worlds = {};
-  let serverMap = {};
-  let groupMap = {};
-  let worldMap = {};
-  let guildMap = {};
-  let guildWorldIdSet = new Set();
-  let regionId = 1;
-  let groupId = 0;
-  let worldId = 0;
-
-  const loadWorld = async () => {
+  static async Request(path){
     try {
-      const [wgroupsRes, worldsRes] = await Promise.all([
-        fetch(`${API_URL}/wgroups`),
-        fetch(`${API_URL}/worlds`),
-      ]);
+      const response = await fetch(`${this.API_URL}/${path}`);
 
-      if (!wgroupsRes.ok || !worldsRes.ok) {
-        throw new Error("1つ以上のAPI呼び出しに失敗しました");
+      if (!response.ok){
+        //todo: error handling
+        throw new Error('Api Error');
       }
 
-      const [wgroupsData, worldsData] = await Promise.all([
-        wgroupsRes.json(),
-        worldsRes.json(),
-      ]);
+      return await response.json();
 
-      groups = wgroupsData;
-      worlds = worldsData;
-
-      serverMap = {};
-      for (const world of worlds.data){
-        const regionId = Math.floor(world.world_id / 1000);
-        if (!(regionId in serverMap)){
-          serverMap[regionId] = world.server;
-        }
-      }
-
-      groupMap = {};
-      worldMap = {};
-      for (const group of groups.data){
-        groupMap[group.group_id] = group.worlds;
-        for (const worldId of group.worlds){
-          worldMap[worldId] = group.group_id;
-        }
-      }
-
-      submitButtonTextElement.text('refresh');
-
-    } catch (error) {
+    } catch (error){
       console.error(error);
     }
   };
 
-  const loadGuildRanking = async () => {
+  static async Requests(paths){
     try {
-      const group = groupMap[groupId];
-      if (group == undefined){
-        return;
+      const responses = await Promise.all(paths.map(path => fetch(`${this.API_URL}/${path}`)));
+
+      if (responses.some(response => !response.ok)){
+        //todo: error handling
+        throw new Error('Api Error');
       }
 
-      const fetchList = $.map(group, (worldId) => {
-        return fetch(`${API_URL}/${worldId}/guild_ranking/latest`);
-      });
-      const responses = await Promise.all(fetchList);
-      const jsonDataList = await Promise.all($.map(responses, response => response.json()));
+      return await Promise.all($.map(responses, response => response.json()));
 
-      let bpList = [];
-      $.each(jsonDataList, (_, jsonData) => {
-        const worldId = jsonData.data.world_id;
-        bpList = bpList.concat($.map(jsonData.data.rankings.bp, bpEntry => {
-          return {
-            ...bpEntry,
-            world_id: worldId
-          };
-        }));
-      });
-      bpList.sort((a, b) => b.bp - a.bp);
-
-      clearElementList(rankingCellList);
-
-      for (var index = 0; index < 16; ++index){
-        if (bpList.length <= index){
-          break;
-        }
-
-        var v = bpList[index];
-        createGuildCell(rankingCellContainer1, index + 1, v.world_id, v.name, v.id, v.bp);
-      }
-
-      for (var index = 16; index < 32; ++index){
-        if (bpList.length <= index){
-          break;
-        }
-
-        var v = bpList[index];
-        createGuildCell(rankingCellContainer2, index + 1, v.world_id, v.name, v.id, v.bp);
-      }
-
-      for (var index = 32; index < 48; ++index){
-        if (bpList.length <= index){
-          break;
-        }
-
-        var v = bpList[index];
-        createGuildCell(rankingCellContainer3, index + 1, v.world_id, v.name, v.id, v.bp);
-      }
-
-      contentExplain.hide();
-      contentRankings.show();
-      
-
-    } catch (error) {
+    } catch (error){
       console.error(error);
     }
   };
+}
 
-  const loadPlayerRanking = async (worldId, guildId, guildName) => {
+class Data {
+  groups = {};
+  worlds = {};
+  regionMap = {};
+  groupMap = {};
+  worldMap = {};
+  guildMap = {};
+  guildWorldIdSet = new Set();
+  regionId = 1;
+  groupId = 0;
+  worldId = 0;
 
-    if (!guildWorldIdSet.has(worldId)){
-      const response = await fetch(`${API_URL}/${worldId}/player_ranking/latest`);
-      const jsonData = await response.json();
+  updateGroupId(){
+    this.groupId = this.worldMap[this.worldId + this.regionId * 1000] ?? 0;
+  }
+
+  setWorldId(id){
+    if (isNaN(id)) {
+      return;
+    }
+    this.worldId = id;
+    this.updateGroupId();
+  }
+
+  setRegionId(id){
+    if (isNaN(id)) {
+      return;
+    }
+    this.regionId = id;
+    this.updateGroupId();
+  }
+
+  async loadWorld(){
+    const [wgroupsData, worldsData] = await Api.Requests(['wgroups', 'worlds']);
+
+    this.groups = wgroupsData;
+    this.worlds = worldsData;
+
+    this.regionMap = {};
+    for (const world of this.worlds.data){
+      const regionId = Math.floor(world.world_id / 1000);
+      if (!(regionId in this.regionMap)){
+        this.regionMap[regionId] = world.server;
+      }
+    }
+
+    this.groupMap = {};
+    this.worldMap = {};
+    for (const group of this.groups.data){
+      this.groupMap[group.group_id] = group.worlds;
+      for (const worldId of group.worlds){
+        this.worldMap[worldId] = group.group_id;
+      }
+    }
+  };
+
+  async loadGuildRanking(){
+    const group = this.groupMap[this.groupId];
+    if (group == undefined){
+      return [];
+    }
+    
+    const jsonDataList = await Api.Requests(group.map(worldId => `${worldId}/guild_ranking/latest`));
+
+    let bpList = [];
+    $.each(jsonDataList, (_, jsonData) => {
+      const worldId = jsonData.data.world_id;
+      bpList = bpList.concat($.map(jsonData.data.rankings.bp, bpEntry => {
+        return {
+          ...bpEntry,
+          world_id: worldId
+        };
+      }));
+    });
+    bpList.sort((a, b) => b.bp - a.bp);
+
+    return bpList;
+  };
+
+  async loadPlayerRanking(worldId, guildId){
+    if (!this.guildWorldIdSet.has(worldId)){
+      const jsonData = await Api.Request(`${worldId}/player_ranking/latest`);
       const players = Object.values(jsonData.data.player_info).sort((a, b) => b.bp - a.bp);
 
-      Object.assign(guildMap, players.reduce((groups, player) => {
+      Object.assign(this.guildMap, players.reduce((groups, player) => {
         const guildId = player.guild_id;
         if (!groups[guildId]) {
           groups[guildId] = [];
@@ -153,26 +129,49 @@ $(document).ready(async () => {
         return groups;
       }, {}));
 
-      guildWorldIdSet.add(worldId);
+      this.guildWorldIdSet.add(worldId);
     }
 
-    clearElementList(bpCellList);
+    return this.guildMap[guildId];
+  };
+}
 
-    let count = 0;
-    for (const player of guildMap[guildId]){
-      createPlayerCell(bpCellContainer, ++count, player.name, player.bp);
+class UI {
+  rankingCellTemplate = document.querySelector('#ranking-cell');
+  guildRankingCellContainer1 = $('#ranking-cells-1');
+  guildRankingCellContainer2 = $('#ranking-cells-2');
+  guildRankingCellContainer3 = $('#ranking-cells-3');
+  playerRankingCellContainer = $('#bp-cells');
+  worldIdInputField = $('#number_input');
+  submitButtonLabel = $('#Submit i.material-icons');
+  headerTitleLabel = $('#header-title');
+  guildNameLabel = $('#guild-name');
+  groupLabel = $('#group-label')
+  contentExplain = $('#content-explain');
+  contentRankings = $('#content-rankings');
+  contentBpList = $('#content-bplist');
+
+  guildRankingCellList = [];
+  playerRankingCellList = [];
+
+  constructor(){
+    M.AutoInit();
+    this.contentExplain.hide();
+    this.contentBpList.hide();
+    this.contentRankings.hide();
+    this.contentExplain.fadeIn(500);
+  }
+
+  updateGroupLabel(groupId){
+    if (groupId < 1){
+      this.groupLabel.text('');
+      return;
     }
 
-    guildNameElement.text(guildName);
-
-    //
-    contentRankings.hide();
-    contentBpList.show();
-    submitButtonTextElement.text('close');
-    headerTitleElement.text('何らかのランキングにエントリーしているプレーヤーのリスト');
+    this.groupLabel.text(`Group${groupId}`);
   };
 
-  const clearElementList = (list) => {
+  clearElementList(list){
     for (const cell of list) {
       if (cell.parentNode) {
         cell.parentNode.removeChild(cell);
@@ -181,98 +180,117 @@ $(document).ready(async () => {
     list.length = 0;
   };
 
-  const createGuildCell = (container, rank, worldId, guildName, guildId, bp) => {
-    const clone = rankingCellTemplate.content.cloneNode(true);
+  createGuildCell(container, rank, worldId, guildName, guildId, bp, serverName, onSelect){
+    const clone = this.rankingCellTemplate.content.cloneNode(true);
     const li = clone.querySelector('li');
     li.querySelector('.rankcell-rank').innerHTML = `${rank}`;
-    li.querySelector('.rankcell-world').innerHTML = `${serverMap[Math.floor(worldId / 1000)]}${worldId % 1000}`;
+    li.querySelector('.rankcell-world').innerHTML = serverName;
     li.querySelector('.rankcell-guildname').innerHTML = guildName;
     li.querySelector('.rankcell-bp').innerHTML = Number(bp).toLocaleString();
-    li.addEventListener('click', () => {
-      loadPlayerRanking(worldId, guildId, guildName);
-    });
-    rankingCellList.push(li);
+    li.addEventListener('click', onSelect);
+    this.guildRankingCellList.push(li);
     container.append(clone);
   };
 
-  const createPlayerCell = (container, rank, playerName, bp) => {
-    const clone = rankingCellTemplate.content.cloneNode(true);
+  createPlayerCell(container, rank, playerName, bp){
+    const clone = this.rankingCellTemplate.content.cloneNode(true);
     const li = clone.querySelector('li');
     li.querySelector('.rankcell-rank').innerHTML = `${rank}`;
     li.querySelector('.rankcell-world').innerHTML = ``;
     li.querySelector('.rankcell-guildname').innerHTML = playerName;
     li.querySelector('.rankcell-bp').innerHTML = Number(bp).toLocaleString();
-    bpCellList.push(li);
+    this.playerRankingCellList.push(li);
     container.append(clone);
   };
+}
 
-  const updateGroupLabel = () => {
-    groupId = worldMap[worldId + regionId * 1000];
-    if (groupId == undefined){
-      groupId = 0;
-    }
+$(document).ready(async () => {
+  const WORLD_ID_KEY = "world_id";
 
-    if (groupId < 0){
-      groupLabel.text('');
-      return;
-    }
+  const data = new Data();
+  const ui = new UI();
 
-    groupLabel.text(`Group${groupId}`);
-  };
-
-  contentExplain.hide();
-  contentExplain.fadeIn(500);
-  contentRankings.hide();
-  contentBpList.hide();
-
-  M.AutoInit();
-  await loadWorld();
+  await data.loadWorld();
+  ui.submitButtonLabel.text('refresh');
 
   // World number input field
-  worldIdInputField.on('input', function(){
-    const val = parseInt($(this).val(), 10);
-    if (isNaN(val)) {
-      return;
-    }
-    worldId = val;
-    updateGroupLabel();
+  ui.worldIdInputField.on('input', function(){
+    data.setWorldId(parseInt($(this).val(), 10));
+    ui.updateGroupLabel(data.groupId);
   });
 
   const value = localStorage.getItem(WORLD_ID_KEY);
   if (value != undefined){
-    worldIdInputField.val(value);
-    worldIdInputField.siblings('label').addClass('active');
-    worldId = parseInt(value, 10);
-    updateGroupLabel();
+    data.setWorldId(parseInt(value, 10));
+    ui.worldIdInputField.val(value);
+    ui.worldIdInputField.siblings('label').addClass('active');
+    ui.updateGroupLabel(data.groupId);
   }
 
-  // Submit button
-  $('#Submit').on('click', function(event) {
-    if (submitButtonTextElement.text() == 'close'){
-      contentBpList.hide();
-      contentRankings.show();
-      submitButtonTextElement.text('refresh');
-      headerTitleElement.text('Group Guild Bp Ranking');
+  // // Submit button
+  $('#Submit').on('click', async (event) => {
+    if (ui.submitButtonLabel.text() == 'close'){
+      ui.contentBpList.hide();
+      ui.contentRankings.show();
+      ui.submitButtonLabel.text('refresh');
+      ui.headerTitleLabel.text('Group Guild Bp Ranking');
     }else{
-      localStorage.setItem(WORLD_ID_KEY, worldId);
-      loadGuildRanking();
+      localStorage.setItem(WORLD_ID_KEY, data.worldId);
+      const bpList = await data.loadGuildRanking();
+
+      const createGuildRanking = (container, startIndex) => {
+        for (var index = startIndex; index < startIndex + 16; ++index){
+          if (bpList.length <= index){
+            break;
+          }
+
+          const v = bpList[index];
+          const serverName = `${data.regionMap[Math.floor(v.world_id / 1000)]}${v.world_id % 1000}`;
+          ui.createGuildCell(container, index + 1, v.world_id, v.name, v.id, v.bp, serverName, async () => {
+
+            const playerBpList = await data.loadPlayerRanking(v.world_id, v.id);
+
+            ui.clearElementList(ui.playerRankingCellList);
+
+            let count = 0;
+            for (const player of playerBpList){
+              ui.createPlayerCell(ui.playerRankingCellContainer, ++count, player.name, player.bp);
+            }
+
+            ui.guildNameLabel.text(v.name);
+
+            ui.contentRankings.hide();
+            ui.contentBpList.show();
+            ui.submitButtonLabel.text('close');
+            ui.headerTitleLabel.text('何らかのランキングにエントリーしているプレーヤーのリスト');
+          });
+        }
+      };
+
+      ui.clearElementList(ui.guildRankingCellList);
+      createGuildRanking(ui.guildRankingCellContainer1, 0);
+      createGuildRanking(ui.guildRankingCellContainer2, 16);
+      createGuildRanking(ui.guildRankingCellContainer3, 32);
+
+      ui.contentExplain.hide();
+      ui.contentRankings.show();
     }
   });
 
-  // Dropdown Server
+  // // DropDown Region
   const $dropdown = $('#dropdown-servers');
   $dropdown.empty();
-  for (const key in serverMap){
-    $dropdown.append(`<li><a href="#!">${serverMap[key]}</a></li>`);
+  for (const key in data.regionMap){
+    $dropdown.append(`<li><a href="#!">${data.regionMap[key]}</a></li>`);
   }
 
   $('.dropdown-trigger').dropdown();
 
-  $('#dropdown-servers li a').click(function () {
+  $('#dropdown-servers li a').click(function(){
     const selectedText = $(this).text();
     $('#dropdownButton').contents().first()[0].nodeValue = selectedText;
 
-    regionId = Object.keys(serverMap).find(key => serverMap[key] === selectedText);
-    updateGroupLabel();
+    data.setRegionId(Object.keys(data.regionMap).find(key => data.regionMap[key] === selectedText));
+    ui.updateGroupLabel(data.groupId);
   });
 });
